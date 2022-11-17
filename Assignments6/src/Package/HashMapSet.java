c7ac6c3f59942ee3db6110d7da3b16bcf7c43fab
Anudip <Anudip@DESKTOP-63E482B>
2022-11-17 15:44:20 +0500

    Done

---
+++ Assignments6/src/Package/HashMapSet.java
@@ -0,0 +1,21 @@
+package Package;
+
+import java.util.HashMap;
+
+public class HashMapSet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	  HashMap<Integer,Integer> Sc =new HashMap<>();
+		for (int i=1;i<=15;i++)
+		{
+			int SQU = i*i;
+			Sc.put(i, SQU);
+		}
+
+		System.out.println(Sc); 	
+
+	}
+
+}
